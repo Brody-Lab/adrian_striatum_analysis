834ba89f220cda83e4616c141eaa923798258573
Adrian Bondy <adrian.bondy@gmail.com>
2020-11-30 12:00:00 -0500

    updates to recordings log

--- package_cells_for_tiger.m
+++ package_cells_for_tiger.m
@@ -9,6 +9,7 @@ function package_cells_for_tiger(varargin)
     curated_cells_files = recordings_table.curated_cells_file(recordings_table.striatum_glm==1);
     cells_files = recordings_table.cells_file(recordings_table.striatum_glm==1);
     cells_files(~ismissing(curated_cells_files))=curated_cells_files(~ismissing(curated_cells_files));
+    cells_files = cells_files(~ismissing(cells_files));
     fix_path = @(x)strrep(char(x),'"','');
     for i=1:length(cells_files)
         destination=fullfile(P.data_path,'cells',char(regexprep(cells_files(i),'.*Adrian(.*)','$1')));       
