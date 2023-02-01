bd2f328c27a34bb9086f5cc5501cf99ef171e69b
Adrian Bondy <adrian.bondy@gmail.com>
2023-01-31 22:35:11 -0500

    decrease resolution to 1e-2

--- datasets/generate_pset1_data.m
+++ datasets/generate_pset1_data.m
@@ -16,18 +16,18 @@ Cells = add_first_click_state(Cells);
 exclude_trials = validate_trials(Cells.Trials,'mode','agb_glm');
 clicks_on = Cells.Trials.stateTimes.first_click - Cells.Trials.stateTimes.cpoke_in;
 clicks_on = clicks_on(~exclude_trials);
-params = get_pcs(Cells,'resolution_s',5e-3,'trial_idx',~exclude_trials,...
+params = get_pcs(Cells,'resolution_s',1e-2,'trial_idx',~exclude_trials,...
     'exclude_cells',~Cells.is_in_dorsal_striatum);
 params.clicks_on=round(0.5+clicks_on/params.resolution_s);
 params.gamma = Cells.Trials.gamma(~exclude_trials);
 fields=fieldnames(params);
+params.rat_name=char(params.rat);
 mask = {'ref_event','resolution_s','time_window_s',...
     'trial_idx','units','time_s','cells_mat','sessid','rat','sess_date','times','clicks_on','gamma'}; % fields to keep
 params = rmfield(params,fields(~ismember(fields,mask)));
 params.spikes = reshape(params.cells_mat,[size(params.times) size(params.cells_mat,2)]);
 params = rmfield(params,{'cells_mat','times'});
 params.sess_date=char(params.sess_date);
-params.rat=char(params.rat);
 params.spikes=permute(params.spikes,[2 1 3]); % now spikes is ntrials X ntimebins X ncells
 params.n_left_clicks = cellfun(@numel,Cells.Trials.leftBups(~exclude_trials));
 params.n_right_clicks = cellfun(@numel,Cells.Trials.rightBups(~exclude_trials));
