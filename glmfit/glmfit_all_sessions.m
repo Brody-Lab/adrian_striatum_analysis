054a69f38043f45624511dabdb853a1b058ff1b9
Adrian Bondy <adrian.bondy@gmail.com>
2023-06-14 10:15:29 -0400

    various improvements

--- glmfit/glmfit_all_sessions.m
+++ glmfit/glmfit_all_sessions.m
@@ -11,11 +11,19 @@ function glmfit_all_sessions(varargin)
     p.addParameter('partition','all',@(x)validateattributes(x,{'char','string'},{'nonempty'}));
     p.addParameter('requeue',false,@(x)validateattributes(x,{'logical'},{'scalar'})); 
     p.addParameter('alpha',1,@(x)validateattributes(x,{'numeric'},{'scalar','nonnegative'})); % elastic net parameter (0=ridge, 1=lasso) 
+    p.addParameter('glmnet_thresh',1e-6,@(x)validateattributes(x,{'numeric'},{'scalar','nonnegative'}));    
+    p.addParameter('jobid',datestr(now,'YYYY_mm_DD_HH_MM_SS'),@(x)validateattributes(x,{'char','string'},{'nonempty'}));
     p.parse(varargin{:});    
     
     %glm fitting params
     params = glmfit_parse_params(varargin{:});
     params = params.Results;
+    
+    try
+        datetime(p.Results.jobid,'InputFormat','yyyy_MM_dd_hh_mm_ss');
+    catch
+        error('job id %s could not be parsed as a date string.',p.Results.jobid);
+    end
     
     paths = get_data_paths('warn_existence',true,varargin{:});
     if P.on_cluster
@@ -31,21 +39,24 @@ function glmfit_all_sessions(varargin)
     if ~all([paths.all_exist])
         error('Cannot continue because database is not fully accessible.');
     end
-    time_string =datestr(now,'YYYY_mm_DD_HH_MM_SS');
     for i=1:length(paths)
         fprintf('\n---------Submitting job %g of %g-----------\n',i,length(paths));
-        job_name = strcat(paths(i).recording_name,'_glmfit_',time_string);
+        job_name = strcat(paths(i).recording_name,'_glmfit_',p.Results.jobid);
         fprintf('Job name will be %s.\n',job_name);
-        output_dir=fullfile(strrep(paths(i).parent_dir,'cells','fits'),['glmfit_',time_string]);
-        mkdir(output_dir);
-        fprintf('   Made output directory: %s\n   ',output_dir); 
+        output_dir=fullfile(strrep(paths(i).parent_dir,'cells','fits'),['glmfit_',p.Results.jobid]);
+        if isfolder(output_dir)
+            fprintf('   Output directory already exists: %s\n   ',output_dir);             
+        else
+            mkdir(output_dir);
+            fprintf('   Made output directory: %s\n   ',output_dir); 
+        end
         if p.Results.job_array && P.on_cluster
             matlab_command = sprintf(['fit_glm_to_Cells(''%s'',''save_path'',''%s'',''save'',true,''bin_size_s'',%0.10g,',...
-                '''kfold'',%g,''phi'',%0.10g,''tau_phi'',%0.10g,''choice_time_back_s'',%0.10g,''alpha'',%0.10g,'...
+                '''kfold'',%g,''phi'',%0.10g,''tau_phi'',%0.10g,''choice_time_back_s'',%0.10g,''alpha'',%0.10g,''glmnet_thresh'',%0.10g,',...
                 '''distribution'',''%s'',''include_mono_clicks'',logical(%g),''use_trial_history'',logical(%g),''nClickBins'',%g,',...
                 '''separate_clicks_by'',''%s'',''separate_clicks_by_outcome'',logical(%g));'],...
-                paths(i).cells_file,output_dir,params.bin_size_s,params.kfold,params.phi,...
-                params.tau_phi,params.choice_time_back_s,params.alpha,params.distribution,params.include_mono_clicks,params.use_trial_history,...
+                paths(i).cells_file,output_dir,params.bin_size_s,params.kfold,params.phi,params.tau_phi,params.choice_time_back_s,...
+                params.alpha,params.glmnet_thresh,params.distribution,params.include_mono_clicks,params.use_trial_history,...
                 params.nClickBins,params.separate_clicks_by,params.separate_clicks_by_outcome);
             save_param_command=[matlab_command(1:end-2),',''fit'',false);'];
             fprintf('Running fit_glm_to_Cells to save params before batch fitting.\n');
@@ -71,11 +82,11 @@ function glmfit_all_sessions(varargin)
             error_file = fullfile(output_dir,'job%A.stderr');
             out_file = fullfile(output_dir,'job%A.stdout');             
             matlab_command = sprintf(['"fit_glm_to_Cells(''%s'',''save_path'',''%s'',''save'',true,''bin_size_s'',%g,',...
-                '''kfold'',%g,''phi'',%0.10g,''tau_phi'',%0.10g,''choice_time_back_s'',%0.10g,''alpha'',%0.10g,'...
+                '''kfold'',%g,''phi'',%0.10g,''tau_phi'',%0.10g,''choice_time_back_s'',%0.10g,''alpha'',%0.10g,''glmnet_thresh'',%0.10g,'...
                 '''distribution'',''%s'',''include_mono_clicks'',logical(%g),''use_trial_history'',logical(%g),''nClickBins'',%g,',...
                 '''separate_clicks_by'',''%s'',''separate_clicks_by_outcome'',logical(%g));exit;"'],...
-                paths(i).cells_file,output_dir,params.bin_size_s,params.kfold,params.phi,params.tau_phi,...
-                params.choice_time_back_s,params.alpha,params.distribution,params.include_mono_clicks,params.use_trial_history,params.nClickBins,...
+                paths(i).cells_file,output_dir,params.bin_size_s,params.kfold,params.phi,params.tau_phi,params.choice_time_back_s,params.alpha,...
+                params.glmnet_thresh,params.distribution,params.include_mono_clicks,params.use_trial_history,params.nClickBins,...
                 params.separate_clicks_by,params.separate_clicks_by_outcome);       
             if p.Results.requeue
                 sbatch_command = sprintf('sbatch -e %s -o %s -t %g -J "%s" --mail-type=FAIL,TIME_LIMIT -p %s --requeue submit_matlab_job.slurm %s',...
@@ -87,7 +98,15 @@ function glmfit_all_sessions(varargin)
         end
         if P.on_cluster
             fprintf('Sending following system command to initiate job:\n   %s\n',sbatch_command);
-            system(sbatch_command);                       
+            while true
+                output = system(sbatch_command);    
+                if output
+                    fprintf('Command failed. Trying again.\n');
+                else
+                    break
+                end
+            end
+                
         else
             eval(matlab_command(2:end-1)); %remove double quotation marks in character sequence
         end
