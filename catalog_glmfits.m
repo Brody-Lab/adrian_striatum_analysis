1fc34ad0f8f75c7af63ec6fefe9909b6be3cf5ac
Adrian Bondy <adrian.bondy@gmail.com>
2023-06-16 16:41:47 -0400

    update catalog_glmfits to deal with neuro_glmnet

--- catalog_glmfits.m
+++ catalog_glmfits.m
@@ -5,11 +5,25 @@ function glmfit_log = catalog_glmfits(varargin)
     p.addParameter('remove_empty_runs',true,@(x)validateattributes(x,{'logical'},{'scalar'}));
     p.addParameter('delete_spikes',false,@(x)validateattributes(x,{'logical'},{'scalar'}));    
     p.addParameter('trim_params',false,@(x)validateattributes(x,{'logical'},{'scalar'}));        
-    p.addParameter('remove_incomplete_runs',true,@(x)validateattributes(x,{'logical'},{'scalar'}));  
+    p.addParameter('remove_missing_mode','sessions',@(x)validatestring(x,{'sessions','cells','none'}));
+    p.addParameter('delete_removed',false,@(x)validateattributes(x,{'logical'},{'scalar'})); 
     p.addParameter('max_days_old',Inf);
     p.parse(varargin{:});
     params=p.Results;
-    P = get_parameters();
+    P = get_parameters();    
+    switch params.remove_missing_mode
+        case 'sessions'
+            fprintf('Runs with missing sessions will not be included in the log.\n');
+        case 'cells'
+            fprintf('Runs with any missing cells will not be included in the log.\n');
+        case 'none'
+            fprintf('All runs, no matter how imcomplete, will be included in the log.');
+    end
+    if params.delete_removed && params.remove_missing_mode~="none"
+        fprintf('Removed runs will be physically deleted from %s.\n',P.fit_path);
+    else
+        fprintf('Nothing will be physically deleted from %s.\n',P.fit_path);        
+    end
     
     %% load basic params of all glm fit runs in database
     [glmfit_params,saved_cells] = find_glmfit_params(params);
@@ -18,7 +32,7 @@ function glmfit_log = catalog_glmfits(varargin)
     glmfit_log = make_catalog_from_params(glmfit_params,saved_cells);
     
     %% validate catalog
-    glmfit_log = validate_glmfit_log(glmfit_log,params.remove_incomplete_runs);
+    glmfit_log = validate_glmfit_log(glmfit_log,params);
     
     %% save catalog
     fprintf('Saving catalog to %s\n',P.glmfit_catalog_path);tic;
@@ -41,7 +55,7 @@ function [glmfit_params,saved_cells] = find_glmfit_params(params)
     
     %% loop over recordings and fit runs, loading the params file and checking that files have been made for all cells
     % loop over recordings   
-    parfor i=1:length(paths)
+    for i=1:length(paths)
         [glmfit_params{i},saved_cells{i}] = find_glmfit_params_internal(paths(i),params);
     end
     glmfit_params = [glmfit_params{:}];
@@ -74,7 +88,7 @@ function [glmfit_params,saved_cells] = find_glmfit_params_internal(path,params)
             tmp=dir(stats_dir);
             size=[tmp.bytes];
             if sum(size)==0
-                if params.remove_empty_runs % empty stats folder 
+                if params.delete_removed && params.remove_missing_mode~="none"  % empty stats folder 
                     remove_run(run_list{k});
                 end
             else
@@ -93,11 +107,11 @@ function [glmfit_params,saved_cells] = find_glmfit_params_internal(path,params)
                 else
                    error('Params file not found: %s.\n',params_path); % something has gone wrong if you've got to this line.
                 end
-                % check which scells have been saved               
+                % check which cells have been saved               
                 saved_cells{k} = cellfun(@(x)str2double(x),regexprep({tmp(size>0).name},'.*cell(.*).mat','$1'),'uni',0);
                 saved_cells{k}=[saved_cells{k}{:}];
             end
-        elseif params.remove_empty_runs % no stats folder made
+        elseif params.delete_removed && params.remove_missing_mode~="none" % no stats folder made
             remove_run(run_list{k});          
         end
     end 
@@ -108,6 +122,7 @@ function glmfit_log = make_catalog_from_params(glmfit_params,saved_cells)
 
     P = get_parameters();
     default_params = glmfit_parse_params();    
+    default_params_legacy = glmfit_parse_params_legacy();        
 
     %% make params table
     count=0;
@@ -115,6 +130,7 @@ function glmfit_log = make_catalog_from_params(glmfit_params,saved_cells)
     for t=numel(glmfit_params):-1:1 % scalar indexing across all params
         if ~isempty(glmfit_params(t).params)
             count=count+1;
+            T(count).fit_method = "glmnet";            
             for f=1:length(fields)
                 this_field=fields{f};
                 % special cases
@@ -130,6 +146,9 @@ function glmfit_log = make_catalog_from_params(glmfit_params,saved_cells)
                     case 'run'
                         [~,T(count).(this_field)] = fileparts(glmfit_params(t).params.save_path);                            
                     case 'link'
+                        if ~isstruct(glmfit_params(t).params.link)
+                            glmfit_params(t).params.link = make_link(glmfit_params(t).params.link,glmfit_params(t).params.distribution);
+                        end
                         T(count).(this_field) = func2str(glmfit_params(t).params.link.Link);
                     otherwise
                         if isfield(glmfit_params(t).params,this_field)                
@@ -138,13 +157,23 @@ function glmfit_log = make_catalog_from_params(glmfit_params,saved_cells)
                            [~, T(count).(this_field)] = extract_info_from_npx_path(glmfit_params(t).params.save_path);
                         elseif this_field=="lambda_correct_zscore" && glmfit_params(t).params.git_commit=="9e53fd2cb4adf6852328e70cb7a072092c1da944"
                             T(count).lambda_correct_zscore=true;
+                        elseif this_field=="alpha"
+                            T(count).alpha=0;
+                            T(count).fit_method="neuroglmfit";                            
+                        elseif this_field=="foldseed" || this_field=="max_glmnet_attempts" || this_field=="lambda" || this_field=="glmnet_thresh" || this_field=="maxIter"
+                            T(count).(this_field)=NaN;
                         elseif isfield(default_params,this_field)
                             T(count).(this_field) = default_params.(this_field);
+                        elseif isfield(default_params_legacy,this_field)
+                            T(count).(this_field) = default_params_legacy.(this_field);                            
                         end
                 end
                 if isfield(T(count),this_field) && ischar(T(count).(this_field))
                     T(count).(this_field) = string(T(count).(this_field));
                 end
+            end
+            if T(count).git_branch=="neuro_glmnet"
+                T(count).fit_method = "glmnet";
             end
             T(count).dm.dspec.expt = rmfield(T(count).dm.dspec.expt,'trial');            
             % check that all cells have been saved and throw warnings
@@ -173,18 +202,14 @@ function run_list = get_run_list(this_fit_path)
     run_list = cellfun(@(x)fullfile(this_fit_path,x),run_list,'uni',0);
 end
 
-function glmfit_log = validate_glmfit_log(glmfit_log,remove_incomplete_runs)
+function glmfit_log = validate_glmfit_log(glmfit_log,params)
     % checks raw glmfit log (i.e. obtained by a directory search) and
-    % removes runs where all cells and sessions are not represented. if
-    % "remove_incomplete_runs" is true, these files are deleted too, not
+    % optionally removes runs where all cells or sessions are not represented. if
+    % "delete_removed" is true, these files are deleted too, not
     % just removed from the log.
-    % note 3/2022: this has not been extensively tested since incomplete runs
-    % haven't happened in a while. but works when there is a single
-    % complete run.
     [run_idx,unique_runs] = findgroups(glmfit_log.run);
     n_runs = numel(unique_runs); 
-    incomplete = false(n_runs,1);
-    keep_idx = false(height(glmfit_log),1);
+    remove = false(height(glmfit_log),1);
     for i=1:n_runs
         this_idx = run_idx==i;
         %% check that all sessions are represented
@@ -192,26 +217,27 @@ function glmfit_log = validate_glmfit_log(glmfit_log,remove_incomplete_runs)
         missing_sessions = ~ismember(recordings_table.recording_name,glmfit_log.recording_name(this_idx));
         if any(missing_sessions)
             warning('%g recordings are missing from the glmfit log for run %s.',sum(missing_sessions),unique_runs(i));
-            incomplete(i)=true;
+            if params.remove_missing_mode~="none"
+                remove(i)=true;
+            end
         end
         %% check that all cells are represented
         if any(glmfit_log.n_missing_cells(this_idx))
             warning('Missing %g cells from recordings found for run %s.',sum(glmfit_log.n_missing_cells(this_idx)),unique_runs(i));
-            incomplete(i)=true;
-        end
-        if incomplete(i)
-            if remove_incomplete_runs
-                these_recordings = glmfit_log.recording_name(this_idx);
-                run_folders = get_run_folder(repmat(unique_runs(i),numel(these_recordings),1),these_recordings);
-                parfor k=1:numel(run_folders)
-                    remove_run(run_folders(k));
-                end
+            if params.remove_missing_mode=="cells"
+                remove(i)=true;
             end
-        else
-           keep_idx(this_idx)=true; 
+        end
+        %% optionally delete removed runs    
+        if remove(i) && params.delete_removed
+            these_recordings = glmfit_log.recording_name(this_idx);
+            run_folders = get_run_folder(repmat(unique_runs(i),numel(these_recordings),1),these_recordings);
+            for k=1:numel(run_folders)
+                remove_run(run_folders(k));
+            end
         end
     end
-    glmfit_log = glmfit_log(keep_idx,:);
+    glmfit_log = glmfit_log(~remove,:);
 end
 
 function params = trim_params(params)
