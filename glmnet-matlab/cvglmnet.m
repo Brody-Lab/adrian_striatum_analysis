03d5e31252ce0a83d6ec9987d9f2fc2de3c49c76
Adrian Bondy <adrian.bondy@gmail.com>
2023-06-22 13:12:00 -0400

    Update cvglmnet.m

--- glmnet-matlab/cvglmnet.m
+++ glmnet-matlab/cvglmnet.m
@@ -320,9 +320,7 @@ else
         xr = x(~which,:); yr = y(~which,:);
         cpredmat{i} = glmnet(xr, yr, family, opts);
         n_success = numel(cpredmat{i}.lambda);        
-        if jerr<0 
-            cpredmat{i}
-        end
+        cpredmat{i}
         while n_success<opts.nlambda && opts.nlambda>=20
             fprintf('   Removing %dth lambda value for cv fold %d. %d lambdas remaining. \n',n_success+1,i,opts.nlambda-1);
             opts.nlambda = opts.nlambda-1;
