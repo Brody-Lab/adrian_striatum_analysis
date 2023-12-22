cc9385c752cbdc540d3ca20f0fbb016db79f5eb3
Adrian Bondy <adrian.bondy@gmail.com>
2023-12-22 15:54:46 -0500

    remove glmnet-matlab package because in own repo now

--- glmfit/select_cells_and_fits.m
+++ glmfit/select_cells_and_fits.m
@@ -11,6 +11,10 @@ function [fits_table,log] = select_cells_and_fits(glmfit_log,varargin)
 
     P = get_parameters();
     log = select_glmfit_runs(glmfit_log,varargin{:}); % log is for only a single run
+    if isempty(log)
+        fits_table=[];
+        return
+    end
     cells_table = select_cells('is_in_dorsal_striatum',true,varargin{:});
     [fits,is_saved] = get_glm_fits(cells_table.recording_name,cells_table.cellno,log);
     fits_table = [cells_table(is_saved,:) fits];
