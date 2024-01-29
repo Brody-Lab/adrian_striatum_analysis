127812748213d374a157ce85d8283a5e9d96d329
Adrian Bondy <adrian.bondy@gmail.com>
2024-01-29 17:03:54 -0500

    Update estimate_spike_window.m

--- helpers/estimate_spike_window.m
+++ helpers/estimate_spike_window.m
@@ -1,8 +1,13 @@
 function cells_window = estimate_spike_window(Cells,ref_event)
-    n = numel(Cells.raw_spike_time_s);
+    n = numel(Cells.spike_time_s.(ref_event));
     cells_window = [Inf -Inf];
     for c=1:n
-        c_spikes = cat(1,Cells.spike_time_s.(ref_event){c}{:});
+        spks = Cells.spike_time_s.(ref_event){c};
+        if ~isempty(spks)
+            c_spikes = cat(1,spks{:});
+        else
+            c_spikes=[];
+        end
         if ~isempty(c_spikes)
             cells_window(1) = min(min(c_spikes),cells_window(1));
             cells_window(2) = max(max(c_spikes),cells_window(2));     
